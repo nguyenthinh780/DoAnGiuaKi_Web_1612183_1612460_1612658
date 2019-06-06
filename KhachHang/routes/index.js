56caad926eb0c9218fd31417826b6390dfaf6fcd
chau van nhat <chauvannhat11@gmail.com>
2019-06-06 15:59:15 +0700

    Thay doi web khach hang theo mo hinh MVC

--- KhachHang/routes/index.js
+++ KhachHang/routes/index.js
@@ -3,32 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {page:'Home', menuId:'home'});
-});
-
-router.get('/iphonesxmax', function(req, res, next) {
-  res.render('iphoneSX', {page:'iphoneSX', menuId:'iphoneSX'});
-});
-
-router.get('/iphone8', function(req, res, next) {
-  res.render('iphone8', {page:'iphone8', menuId:'iphone8'});
-});
-
-router.get('/note10', function(req, res, next) {
-  res.render('note10', {page:'note10', menuId:'note10'});
-});
-
-router.get('/cart', function(req, res, next) {
-  res.render('cart', {page:'cart', menuId:'cart'});
-});
-router.get('/login', function(req, res, next) {
-  res.render('login', {page:'login', menuId:'login'});
-});
-router.get('/signup', function(req, res, next) {
-  res.render('signup', {page:'signup', menuId:'signup'});
-});
-router.get('/personal', function(req, res, next) {
-  res.render('personal', {page:'personal', menuId:'personal'});
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
